d1dfe9dabf6cd3eed189b9926db1767cf659caf6
yaQot <73448465+myaqut@users.noreply.github.com>
2024-03-20 13:36:21 +0100

    first spring boot application

---
+++ src/main/kotlin/yakout/bankmanagementsystem/BankManagementSystemApplication.kt
@@ -0,0 +1,11 @@
+package yakout.bankmanagementsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BankManagementSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<BankManagementSystemApplication>(*args)
+}
---
+++ src/test/kotlin/yakout/bankmanagementsystem/BankManagementSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package yakout.bankmanagementsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BankManagementSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
